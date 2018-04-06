022d39680fc2d7e8479cad53edb618480e6f6d73
GertjanV <gsverhoeven@users.noreply.github.com>
2018-04-06 15:42:02 +0200

    Add scripts that fit the models and analyse results
    Add compiled pdfs

--- compile_all_pdfs.R
+++ compile_all_pdfs.R
@@ -2,15 +2,19 @@ rm(list = ls())
 
 library(rmarkdown)
 
-# compile all .rmd files
+# compile all .rmd files in the required order
+
 rmarkdown::render("wrangle_data.Rmd", 
                   output_format = "pdf_document", envir = new.env())
-
-
 
 rmarkdown::render("simulate_dynamic.Rmd", 
                   output_format = "pdf_document", envir = new.env())
 
+rmarkdown::render("fit_models.Rmd", 
+                  output_format = "pdf_document", envir = new.env())
+
+rmarkdown::render("analyse_results.Rmd", 
+                  output_format = "pdf_document", envir = new.env())
 
 rmarkdown::render("artificial_turf_predictive.Rmd", 
                   output_format = "pdf_document", envir = new.env())
